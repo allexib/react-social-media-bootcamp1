7e0a6baf8c86b560aa0e3f4945b98475ba0bae74
allexib <ooomegasib@gmail.com>
2021-12-23 15:27:04 +0300

    first step app

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const greeting = <div> Hello </div>;
+const rootNode = document.getElementById('root')
+
+ReactDOM.render(greeting,rootNode)
