0d392edb8242ddea83b0f072a60b00863fac8ba5
allexib <ooomegasib@gmail.com>
2021-12-25 19:58:49 +0300

    sample props2

--- src/index.js
+++ src/index.js
@@ -2,26 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function Header(props) {
-    if (true) {
-        return <h1>hi react {props.username}</h1>
-    }else {
-        return <h1>hi guest</h1>
-    }
-
+    return <h1>hi react {props.username}</h1>
 }
 
 function Layout(props) {
-    return <div style={{background:'gold'}}>
+    return <div style={{background: 'gold'}}>
         {props.children}
     </div>
 }
 
+function Login() {
+    return <p>please login</p>
+}
+
 const rootNode = document.getElementById('root')
 
+const isAuth = true
 
 ReactDOM.render(
     <Layout>
-        <Header username='kama' />
+        {isAuth ? <Header username='kama'/> : <Login/>}
 
     </Layout>,
     rootNode
