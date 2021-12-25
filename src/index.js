2c5bfd4f2738ec88285756121d1bdc9c5ebfb3e1
allexib <ooomegasib@gmail.com>
2021-12-25 20:32:04 +0300

    sample onChange

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import ReactDOM from 'react-dom'
 function App() {
     const people = ['kama', 'dave', 'jane']
 
+    function handleInputChange(event) {
+        const inputValue = event.target.value
+        console.log(inputValue)
+    }
+
     return (
         <ul>
-            {people.map((person,i) => <Person key={i}  person={person}/>)}
+            {people.map((person, i) => <Person key={i} person={person}/>)}
+            <input onChange={handleInputChange}/>
         </ul>
     )
 }
@@ -15,6 +21,7 @@ function App() {
 function Person(props) {
     function handlePersonClick() {
         alert(props.person)
+
     }
 
     return <li onClick={handlePersonClick}>{props.person}</li>
