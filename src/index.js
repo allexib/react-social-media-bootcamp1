8d7087b60560e44955e3e22b06da8156f06b7caa
allexib <ooomegasib@gmail.com>
2021-12-25 20:18:18 +0300

    sample map method

--- src/index.js
+++ src/index.js
@@ -1,37 +1,12 @@
 import React, {Fragment} from 'react'
 import ReactDOM from 'react-dom'
 
-function Header(props) {
-    return <h1>hi react {props.username}</h1>
-}
-
-function Layout(props) {
-    return <div style={{background: 'gold'}}>
-        {props.children}
-    </div>
-}
-
-function Login() {
-    return <p>please login</p>
-}
-
-function Signout() {
-    return <button>signout</button>
-}
-
-
-const isAuth = true
 
 function App() {
-    return (<Layout>
-        {isAuth ? (
-            <>
-                <Header username='kama'/>
-                <Signout/>
-            </>
-        ) : <Login/>}
-        {/*{isAuth && <Signout/>}*/}
-    </Layout>)
+    const people = ['kama', 'dave', 'jane']
+
+    return people.map(person => <p>{person}</p>)
+
 }
 
 const rootNode = document.getElementById('root')
