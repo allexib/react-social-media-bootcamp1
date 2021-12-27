3149d6ded502203f03481bc66a0e10a8c97fe5f7
allexib <ooomegasib@gmail.com>
2021-12-27 18:05:26 +0300

    sample useState4

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom'
 
 
 function App() {
-     const [language,setLanguage] = React.useState('python')
+    const [language, setLanguage] = React.useState('python')
+    const [yearsEperience, setYearsExperience] = React.useState(0)
 
     return (
         <div>
-            <button onClick={()=>setLanguage('js')}>chang lang</button>
+            <button onClick={() => setLanguage('js')}>chang lang</button>
+            <div>
+                <input
+                    type='number'
+                    onChange={event => setYearsExperience(event.target.value)}
+                />
+            </div>
+
             <p>i am learning {language}</p>
+            <p>i have {yearsEperience} years of experience</p>
         </div>
     )
 }
