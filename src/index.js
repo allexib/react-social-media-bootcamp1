44e9720dc7e2fd4ae108a89eb3d3fe36ab95a446
allexib <ooomegasib@gmail.com>
2021-12-27 21:57:58 +0300

    sample useState7,work with local state

--- src/index.js
+++ src/index.js
@@ -4,32 +4,52 @@ import ReactDOM from 'react-dom'
 
 function App() {
     const [developer, setDeveloper] = React.useState({
-        language:'python',
-        yearsExperience:0
+        language: 'python',
+        yearsExperience: 0,
+        isEmployed: false
 
     })
 
     // const [language, setLanguage] = React.useState('python')
     // const [yearsExperience, setYearsExperience] = React.useState(0)
 
+    function handleChangeLanguage() {
+        setDeveloper({
+            language: 'js',
+            yearsExperience: 0
+        })
+    }
+
+    function handleChangeYearsExperience(event) {
+        setDeveloper({
+            ...developer,
+            yearsExperience: event.target.value
+        })
+    }
+
+    function handleToggleEmployment() {
+        setDeveloper(prevState => ({
+            ...prevState,
+            isEmployed: !prevState.isEmployed
+        }))
+    }
+
     return (
         <div>
-            <button onClick={() => setDeveloper({
-                language:'js',
-                yearsExperience:0
-            })}>chang lang</button>
+            <button onClick={handleToggleEmployment}>Toggle Employment Status</button>
+            <button onClick={handleChangeLanguage}
+            >chang lang
+            </button>
             <div>
                 <input
                     type='number'
-                    onChange={event => setDeveloper({
-                        ...developer,
-                        yearsExperience:event.target.value
-                    })}
+                    onChange={handleChangeYearsExperience}
                 />
             </div>
 
             <p>i am learning {developer.language}</p>
             <p>i have {developer.yearsExperience} years of experience</p>
+            <p>Employment status{developer.isEmployed?'Employed':'Unemployed'}</p>
         </div>
     )
 }
