33aeaaaf170988a9e5a82078fdf895aa4ac62a5f
allexib <ooomegasib@gmail.com>
2021-12-29 12:17:44 +0300

    sample useEffect, async await git endpoint 2

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
 import React, {Fragment} from 'react'
 import ReactDOM from 'react-dom'
 
-const endpoint = 'https://api.github.com/users/allexib'
+const baseUrl = 'https://api.github.com/users/'
 
 function App() {
+    const [username, setUsername] = React.useState('allexib')
     const [user, setUser] = React.useState(null)
 
     React.useEffect(() => {
-        async function getUser() {
-            const response = await fetch(endpoint)
+        async function getUser(route) {
+            const response = await fetch(`${baseUrl}${route}`)
             const data = await response.json()
             setUser(data)
         }
-        getUser()
 
+        getUser(username)
         // fetch(endpoint)
         //     .then(response => response.json())
         //     .then(data => setUser(data))
     }, [])
 
-
-    return user ? (
+    return (
         <div>
-            <h2>{user.name}</h2>
-            <p>{user.bio}</p>
-            <img alt='avatar' src={user.avatar_url} style={{height: 350}}/>
+            <input type='text' placeholder='input username'
+                   onChange={event => setUsername(event.target.value)}/>
+            <button>search</button>
+            <button>clear</button>
+            {user ? (
+                <div>
+
+                    <h2>{user.name}</h2>
+                    <p>{user.bio}</p>
+                    <img alt='avatar' src={user.avatar_url} style={{height: 350}}/>
+                </div>
+            ) : (
+                <p>loading</p>
+            )}
         </div>
-    ) : (
-        <p>loading</p>
     )
 }
 
