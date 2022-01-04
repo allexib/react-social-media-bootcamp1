ab7ffade8345d3a374bfc8687eca9299a74ef0c7
allexib <ooomegasib@gmail.com>
2022-01-04 12:42:12 +0300

    sample work with create post\add img refactoring

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
-function CreatePost() {
+function CreatePost({user, setPosts, posts}) {
     const [content, setContent] = React.useState('')
     const [image, setImage] = React.useState(null)
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const post = {content, image, user}
+        const newPost=[post, ...posts]
+        setPosts(newPost)
+
+    }
 
     return (
         <div>
             <h1>Create New Post</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type='text' placeholder='add post content'
                        onChange={event => setContent(event.target.value)}
                 />
@@ -16,11 +24,6 @@ function CreatePost() {
                 />
                 <button type='submit'>submit post</button>
             </form>
-            <p>{content}</p>
-            {image && (<img
-                style={{height: 300, width: 300, objectFit: 'cover'}}
-                src={URL.createObjectURL(image)}
-            />)}
         </div>
     )
 }
