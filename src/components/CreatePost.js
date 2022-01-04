9417504a0e8b72ed6ffdcfb0860d2d5a74b7de14
allexib <ooomegasib@gmail.com>
2022-01-04 12:49:41 +0300

    sample work with create post\add img

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,28 @@
 import React from 'react'
 
 function CreatePost() {
-    return <div>create post</div>
+    const [content, setContent] = React.useState('')
+    const [image, setImage] = React.useState(null)
+
+    return (
+        <div>
+            <h1>Create New Post</h1>
+            <form>
+                <input type='text' placeholder='add post content'
+                       onChange={event => setContent(event.target.value)}
+                />
+                <input type='file'
+                       onChange={event => setImage(event.target.files[0])}
+                />
+                <button type='submit'>submit post</button>
+            </form>
+            <p>{content}</p>
+            {image && (<img
+                style={{height: 300, width: 300, objectFit: 'cover'}}
+                src={URL.createObjectURL(image)}
+            />)}
+        </div>
+    )
 }
 
 export default CreatePost
