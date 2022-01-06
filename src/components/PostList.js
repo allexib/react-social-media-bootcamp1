6ca279c6e723c30674cebe575785678121dd1c75
allexib <ooomegasib@gmail.com>
2022-01-06 10:18:29 +0300

    make clean app,PostL conponent work

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,18 +1,9 @@
 import React from 'react'
+import Post from './Post'
 
 function PostList({posts}) {
-    return posts.map((post, i) => (
-        <React.Fragment key={i}>
-            {post.image && (
-                <img
-                    style={{height: 200, width: 300, objectFit: 'cover'}}
-                    src={URL.createObjectURL(post.image)}
-                    alt='post cover'
-                />
-            )}
-            <p> {post.content}</p>
-            <div>{post.user}</div>
-        </React.Fragment>
+    return posts.map((post, i) => (<Post key={i} post={post}/>
+
     ))
 }
 
