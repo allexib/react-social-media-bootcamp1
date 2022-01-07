0b547d6554684505eb58258cf710cd2c6853b566
allexib <ooomegasib@gmail.com>
2022-01-07 11:07:11 +0300

    UserContex,consumer,provider awoid props drilling2

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,27 +2,23 @@ import React from 'react'
 import {UserContext} from "../App";
 
 function Post({image, content, user}) {
-    //const isCurrentUser = currentUser === user
+    const currentUser = React.useContext(UserContext)
+    const isCurrentUser = currentUser === user
 
     return (
-        <UserContext.Consumer>
-            {currentUser => (
-                <>
-                    {image && (
-                        <img
-                            style={{height: 300, width: 300, objectFit: 'cover'}}
-                            src={URL.createObjectURL(image)}
-                            alt='post cover'
-                        />
-                    )}
-                    <p> {content}</p>
-                    <div style={{color: currentUser === user && 'green'}}>{user}</div>
-                </>
+        <>
+            {image && (
+                <img
+                    style={{height: 300, width: 300, objectFit: 'cover'}}
+                    src={URL.createObjectURL(image)}
+                    alt='post cover'
+                />
             )}
-
-
-        </UserContext.Consumer>
+            <p> {content}</p>
+            <div style={{color: currentUser === user && 'green'}}>{user}</div>
+        </>
     )
 }
+
 
 export default Post
